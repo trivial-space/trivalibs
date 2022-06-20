d51cb0d946d944dc59328d1efc58a491f214570a
Thomas Gorny <thomas.gorny@pm.me>
2022-06-20 10:28:54 +0200

    start implementing line2d buffered geometry

--- src/geometry/line_2d/buffer_geometry.rs
+++ src/geometry/line_2d/buffer_geometry.rs
@@ -1,9 +1,31 @@
-use crate::rendering::buffered_geometry::ToBufferedGeometry;
-
 use super::Line;
+use crate::{
+    rendering::buffered_geometry::{BufferedGeometry, ToBufferedGeometry},
+    utils::default,
+};
+use std::f32::consts::PI;
+
+pub struct LineGeometryOpts {
+    pub split_angle_threshold: f32,
+}
+
+impl Default for LineGeometryOpts {
+    fn default() -> Self {
+        Self {
+            split_angle_threshold: PI * 0.666,
+        }
+    }
+}
 
 impl ToBufferedGeometry for Line {
-    fn to_buffered_geometry(&self) -> crate::rendering::buffered_geometry::BufferedGeometry {
+    fn to_buffered_geometry(&self) -> BufferedGeometry {
+        self.to_buffered_geometry_with(default())
+    }
+}
+
+impl Line {
+    pub fn to_buffered_geometry_with(&self, opts: LineGeometryOpts) -> BufferedGeometry {
+        let line_parts = self.split_at_angle(opts.split_angle_threshold);
         todo!()
     }
 }
