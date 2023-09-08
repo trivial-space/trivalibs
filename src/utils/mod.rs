e7ffd2b4c2920b49966f9a2d18597545ecb02d8f
Thomas Gorny <thomas.gorny@pm.me>
2023-09-08 13:44:26 +0200

    add random helpers

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,27 @@
+use glam::{vec3, Vec3};
+use rand::random;
+
 pub mod app_state;
 
 pub fn default<T: Default>() -> T {
     std::default::Default::default()
+}
+
+pub fn random_range(min: f32, max: f32) -> f32 {
+    min + (max - min) * random::<f32>()
+}
+
+pub fn random_in_unit_sphere() -> Vec3 {
+    loop {
+        let p = vec3(
+            random_range(-1.0, 1.0),
+            random_range(-1.0, 1.0),
+            random_range(-1.0, 1.0),
+        );
+
+        let ls = p.length_squared();
+        if ls < 1.0 && ls > 0.0000001 {
+            return p;
+        }
+    }
 }
