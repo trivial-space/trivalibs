9fc68bf0e4d85af39764531d983f7b693b0272ab
Thomas Gorny <thomas.gorny@pm.me>
2023-12-24 11:59:06 +0100

    refactor random utils

---
+++ src/utils/rand_utils.rs
@@ -0,0 +1,40 @@
+use glam::{vec3, Vec3};
+use rand::random;
+
+pub fn random_range(min: f32, max: f32) -> f32 {
+    min + (max - min) * random::<f32>()
+}
+
+pub fn rand_int(max: usize) -> usize {
+    (random::<f32>() * max as f32).floor() as usize
+}
+
+pub fn random_in_unit_sphere() -> Vec3 {
+    loop {
+        let p = vec3(
+            random_range(-1.0, 1.0),
+            random_range(-1.0, 1.0),
+            random_range(-1.0, 1.0),
+        );
+
+        let ls = p.length_squared();
+        if ls < 1.0 && ls > 0.0000001 {
+            return p;
+        }
+    }
+}
+
+/// Returns a random number in the range [-1, 1] with normal distribution.
+pub fn random_normal() -> f32 {
+    (random::<f32>() + random::<f32>() + random::<f32>()) / 1.5 - 1.0
+}
+
+pub trait Pick<T> {
+    fn pick(&self) -> &T;
+}
+
+impl<T> Pick<T> for &[T] {
+    fn pick(&self) -> &T {
+        &self[(random::<f32>() * self.len() as f32).floor() as usize]
+    }
+}
