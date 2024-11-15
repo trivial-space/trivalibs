0332757bc3051217924240fe35cbbafa5c4f8afd
Thomas Gorny <thomas.gorny@pm.me>
2024-11-16 00:02:24 +0100

    update glam and some formatting

--- src/geometry/line_2d/mod.rs
+++ src/geometry/line_2d/mod.rs
@@ -8,377 +8,377 @@ use std::slice::Iter;
 #[derive(Clone, Copy)]
 pub struct EmptyData {}
 impl Default for EmptyData {
-    fn default() -> Self {
-        EmptyData {}
-    }
+	fn default() -> Self {
+		EmptyData {}
+	}
 }
 impl<F> Lerp<F> for EmptyData {
-    fn lerp(self, _: Self, _: F) -> Self {
-        EmptyData {}
-    }
+	fn lerp(self, _: Self, _: F) -> Self {
+		EmptyData {}
+	}
 }
 
 #[derive(Clone, Copy)]
 pub struct LineVertexData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32> + Sized,
+	T: Default + Copy + Clone + Lerp<f32> + Sized,
 {
-    pub pos: Vec2,
-    pub width: f32,
-    pub len: f32,
-    pub dir: Vec2,
-    pub data: T,
+	pub pos: Vec2,
+	pub width: f32,
+	pub len: f32,
+	pub dir: Vec2,
+	pub data: T,
 }
 
 pub type LineVertex = LineVertexData<EmptyData>;
 
 impl<T> Default for LineVertexData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    fn default() -> Self {
-        Self {
-            pos: Vec2::ZERO,
-            width: 1.0,
-            len: 0.0,
-            dir: Vec2::ZERO,
-            data: T::default(),
-        }
-    }
+	fn default() -> Self {
+		Self {
+			pos: Vec2::ZERO,
+			width: 1.0,
+			len: 0.0,
+			dir: Vec2::ZERO,
+			data: T::default(),
+		}
+	}
 }
 impl<T> Lerp<f32> for LineVertexData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    fn lerp(self, other: Self, t: f32) -> Self {
-        line_vert_w_d(
-            self.pos.lerp(other.pos, t),
-            self.width.lerp(other.width, t),
-            self.data.lerp(other.data, t),
-        )
-    }
+	fn lerp(self, other: Self, t: f32) -> Self {
+		line_vert_w_d(
+			self.pos.lerp(other.pos, t),
+			Lerp::lerp(self.width, other.width, t),
+			self.data.lerp(other.data, t),
+		)
+	}
 }
 
 impl<T> LineVertexData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    fn new(pos: Vec2) -> Self {
-        LineVertexData { pos, ..default() }
-    }
+	fn new(pos: Vec2) -> Self {
+		LineVertexData { pos, ..default() }
+	}
 
-    pub fn point_to(&mut self, point: &Vec2) {
-        let mut vec = *point - self.pos;
-        let len = vec.length();
-        self.len = len;
-        vec /= len;
-        self.dir = vec;
-    }
+	pub fn point_to(&mut self, point: &Vec2) {
+		let mut vec = *point - self.pos;
+		let len = vec.length();
+		self.len = len;
+		vec /= len;
+		self.dir = vec;
+	}
 
-    pub fn smouth_edge_threshold(
-        &self,
-        prev: &Self,
-        next: &Self,
-        ratio: f32,
-        angle_threshold: f32,
-    ) -> Vec<Self> {
-        let d = 1. - self.dir.dot(prev.dir);
-        if d > angle_threshold {
-            let v1 = prev.lerp(*self, 1.0 - ratio);
-            let v2 = self.lerp(*next, ratio);
+	pub fn smouth_edge_threshold(
+		&self,
+		prev: &Self,
+		next: &Self,
+		ratio: f32,
+		angle_threshold: f32,
+	) -> Vec<Self> {
+		let d = 1. - self.dir.dot(prev.dir);
+		if d > angle_threshold {
+			let v1 = prev.lerp(*self, 1.0 - ratio);
+			let v2 = self.lerp(*next, ratio);
 
-            vec![v1, v2]
-        } else {
-            vec![*self]
-        }
-    }
+			vec![v1, v2]
+		} else {
+			vec![*self]
+		}
+	}
 
-    pub fn smouth_edge(&self, prev: &Self, next: &Self, ratio: f32) -> Vec<Self> {
-        self.smouth_edge_threshold(prev, next, ratio, 0.0)
-    }
+	pub fn smouth_edge(&self, prev: &Self, next: &Self, ratio: f32) -> Vec<Self> {
+		self.smouth_edge_threshold(prev, next, ratio, 0.0)
+	}
 }
 
 pub fn line_vert<T: Default + Copy + Clone + Lerp<f32>>(pos: Vec2) -> LineVertexData<T> {
-    LineVertexData::new(pos)
+	LineVertexData::new(pos)
 }
 
 pub fn line_vert_w<T: Default + Copy + Clone + Lerp<f32>>(
-    pos: Vec2,
-    width: f32,
+	pos: Vec2,
+	width: f32,
 ) -> LineVertexData<T> {
-    LineVertexData {
-        pos,
-        width,
-        ..default()
-    }
+	LineVertexData {
+		pos,
+		width,
+		..default()
+	}
 }
 pub fn line_vert_w_d<T: Default + Copy + Clone + Lerp<f32>>(
-    pos: Vec2,
-    width: f32,
-    data: T,
+	pos: Vec2,
+	width: f32,
+	data: T,
 ) -> LineVertexData<T> {
-    LineVertexData {
-        pos,
-        width,
-        data,
-        ..default()
-    }
+	LineVertexData {
+		pos,
+		width,
+		data,
+		..default()
+	}
 }
 
 #[derive(Clone)]
 pub struct LineData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    list: Vec<LineVertexData<T>>,
-    len: f32,
-    default_width: f32,
-    len_offset: f32,
+	list: Vec<LineVertexData<T>>,
+	len: f32,
+	default_width: f32,
+	len_offset: f32,
 }
 
 pub type Line = LineData<EmptyData>;
 
 impl<T> LineData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    pub fn new_offset(width: f32, offset: f32) -> Self {
-        LineData::<T> {
-            list: Vec::new(),
-            len: 0.0,
-            default_width: width,
-            len_offset: offset,
-        }
-    }
+	pub fn new_offset(width: f32, offset: f32) -> Self {
+		LineData::<T> {
+			list: Vec::new(),
+			len: 0.0,
+			default_width: width,
+			len_offset: offset,
+		}
+	}
 
-    pub fn new(width: f32) -> Self {
-        Self::new_offset(width, 0.0)
-    }
+	pub fn new(width: f32) -> Self {
+		Self::new_offset(width, 0.0)
+	}
 
-    pub fn from_vecs<I: IntoIterator<Item = Vec2>>(line_width: f32, iter: I) -> Self {
-        let mut line = LineData::<T>::new(line_width);
-        for vert in iter {
-            line.add(vert);
-        }
-        line
-    }
+	pub fn from_vecs<I: IntoIterator<Item = Vec2>>(line_width: f32, iter: I) -> Self {
+		let mut line = LineData::<T>::new(line_width);
+		for vert in iter {
+			line.add(vert);
+		}
+		line
+	}
 
-    pub fn line_length(&self) -> f32 {
-        self.len
-    }
+	pub fn line_length(&self) -> f32 {
+		self.len
+	}
 
-    pub fn vert_count(&self) -> usize {
-        self.list.len()
-    }
+	pub fn vert_count(&self) -> usize {
+		self.list.len()
+	}
 
-    pub fn add(&mut self, pos: Vec2) {
-        self.add_vert(line_vert_w(pos, self.default_width));
-    }
+	pub fn add(&mut self, pos: Vec2) {
+		self.add_vert(line_vert_w(pos, self.default_width));
+	}
 
-    pub fn add_width(&mut self, pos: Vec2, width: f32) {
-        self.add_vert(line_vert_w(pos, width));
-    }
+	pub fn add_width(&mut self, pos: Vec2, width: f32) {
+		self.add_vert(line_vert_w(pos, width));
+	}
 
-    pub fn add_width_data(&mut self, pos: Vec2, width: f32, data: T) {
-        self.add_vert(line_vert_w_d(pos, width, data));
-    }
+	pub fn add_width_data(&mut self, pos: Vec2, width: f32, data: T) {
+		self.add_vert(line_vert_w_d(pos, width, data));
+	}
 
-    pub fn add_vert(&mut self, mut vert: LineVertexData<T>) {
-        let curr_len = self.list.len();
+	pub fn add_vert(&mut self, mut vert: LineVertexData<T>) {
+		let curr_len = self.list.len();
 
-        if curr_len > 0 {
-            let idx = curr_len - 1;
-            let prev = &mut self.list[idx];
-            prev.point_to(&vert.pos);
+		if curr_len > 0 {
+			let idx = curr_len - 1;
+			let prev = &mut self.list[idx];
+			prev.point_to(&vert.pos);
 
-            self.len += prev.len;
-            vert.dir = prev.dir;
-        }
+			self.len += prev.len;
+			vert.dir = prev.dir;
+		}
 
-        self.list.push(vert);
-    }
+		self.list.push(vert);
+	}
 
-    pub fn add_vert_raw(&mut self, vert: LineVertexData<T>) {
-        let curr_len = self.list.len();
+	pub fn add_vert_raw(&mut self, vert: LineVertexData<T>) {
+		let curr_len = self.list.len();
 
-        if curr_len > 0 {
-            let idx = curr_len - 1;
-            let prev = &self.list[idx];
-            self.len += prev.len;
-        }
+		if curr_len > 0 {
+			let idx = curr_len - 1;
+			let prev = &self.list[idx];
+			self.len += prev.len;
+		}
 
-        self.list.push(vert);
-    }
+		self.list.push(vert);
+	}
 
-    pub fn iter(&self) -> Iter<'_, LineVertexData<T>> {
-        self.list.iter()
-    }
+	pub fn iter(&self) -> Iter<'_, LineVertexData<T>> {
+		self.list.iter()
+	}
 
-    pub fn get(&self, i: usize) -> &LineVertexData<T> {
-        &self.list[i]
-    }
+	pub fn get(&self, i: usize) -> &LineVertexData<T> {
+		&self.list[i]
+	}
 
-    pub fn get_opt(&self, i: usize) -> Option<&LineVertexData<T>> {
-        self.list.get(i)
-    }
+	pub fn get_opt(&self, i: usize) -> Option<&LineVertexData<T>> {
+		self.list.get(i)
+	}
 
-    pub fn set_raw(&mut self, i: usize, vert: LineVertexData<T>) {
-        self.list[i] = vert;
-    }
+	pub fn set_raw(&mut self, i: usize, vert: LineVertexData<T>) {
+		self.list[i] = vert;
+	}
 
-    pub fn first(&self) -> &LineVertexData<T> {
-        &self.list[0]
-    }
+	pub fn first(&self) -> &LineVertexData<T> {
+		&self.list[0]
+	}
 
-    pub fn last(&self) -> &LineVertexData<T> {
-        &self.list[self.list.len() - 1]
-    }
+	pub fn last(&self) -> &LineVertexData<T> {
+		&self.list[self.list.len() - 1]
+	}
 
-    pub fn split_at_angle(&self, angle_threshold: f32) -> Vec<Self> {
-        let mut lines = vec![];
-        let mut line = LineData::<T>::new(self.default_width);
-        let mut prev: Option<&LineVertexData<T>> = None;
-        let cos_threshold = f32::cos(angle_threshold);
-        let mut len_offset = 0.0;
+	pub fn split_at_angle(&self, angle_threshold: f32) -> Vec<Self> {
+		let mut lines = vec![];
+		let mut line = LineData::<T>::new(self.default_width);
+		let mut prev: Option<&LineVertexData<T>> = None;
+		let cos_threshold = f32::cos(angle_threshold);
+		let mut len_offset = 0.0;
 
-        for v in self {
-            line.add_vert_raw(*v);
+		for v in self {
+			line.add_vert_raw(*v);
 
-            if let Some(prev) = prev {
-                let dot = v.dir.dot(prev.dir);
+			if let Some(prev) = prev {
+				let dot = v.dir.dot(prev.dir);
 
-                if dot <= cos_threshold {
-                    len_offset += line.len;
-                    let mut last = line.last().clone();
-                    last.dir = prev.dir;
-                    line.set_raw(line.list.len() - 1, last);
-                    lines.push(line);
-                    line = LineData::new(self.default_width);
-                    line.len_offset = len_offset;
-                    line.add_vert_raw(*v);
-                }
-            }
-            prev = Some(v);
-        }
+				if dot <= cos_threshold {
+					len_offset += line.len;
+					let mut last = line.last().clone();
+					last.dir = prev.dir;
+					line.set_raw(line.list.len() - 1, last);
+					lines.push(line);
+					line = LineData::new(self.default_width);
+					line.len_offset = len_offset;
+					line.add_vert_raw(*v);
+				}
+			}
+			prev = Some(v);
+		}
 
-        lines.push(line);
-        lines
-    }
+		lines.push(line);
+		lines
+	}
 
-    pub fn flat_map_with_prev_next<
-        F: Fn(
-            &LineVertexData<T>,
-            Option<&LineVertexData<T>>,
-            Option<&LineVertexData<T>>,
-        ) -> Vec<LineVertexData<T>>,
-    >(
-        &self,
-        f: F,
-    ) -> Self {
-        let new_vertices = self.iter().flat_map_with_prev_next(f);
-        LineData::from_iter(new_vertices)
-    }
+	pub fn flat_map_with_prev_next<
+		F: Fn(
+			&LineVertexData<T>,
+			Option<&LineVertexData<T>>,
+			Option<&LineVertexData<T>>,
+		) -> Vec<LineVertexData<T>>,
+	>(
+		&self,
+		f: F,
+	) -> Self {
+		let new_vertices = self.iter().flat_map_with_prev_next(f);
+		LineData::from_iter(new_vertices)
+	}
 
-    pub fn smouth_edges_threshold(&self, ratio: f32, min_dist: f32, angle_threshold: f32) -> Self {
-        self.flat_map_with_prev_next(|curr, prev, next| {
-            if prev.is_none() || next.is_none() {
-                return vec![*curr];
-            }
+	pub fn smouth_edges_threshold(&self, ratio: f32, min_dist: f32, angle_threshold: f32) -> Self {
+		self.flat_map_with_prev_next(|curr, prev, next| {
+			if prev.is_none() || next.is_none() {
+				return vec![*curr];
+			}
 
-            let prev = prev.unwrap();
-            let next = next.unwrap();
+			let prev = prev.unwrap();
+			let next = next.unwrap();
 
-            if prev.len < min_dist || curr.len < min_dist {
-                return vec![*curr];
-            }
+			if prev.len < min_dist || curr.len < min_dist {
+				return vec![*curr];
+			}
 
-            return curr.smouth_edge_threshold(prev, next, ratio, angle_threshold);
-        })
-    }
+			return curr.smouth_edge_threshold(prev, next, ratio, angle_threshold);
+		})
+	}
 
-    pub fn smouth_edges(&self, ratio: f32, min_dist: f32) -> Self {
-        self.smouth_edges_threshold(ratio, min_dist, 0.0)
-    }
+	pub fn smouth_edges(&self, ratio: f32, min_dist: f32) -> Self {
+		self.smouth_edges_threshold(ratio, min_dist, 0.0)
+	}
 
-    pub fn cleanup_vertices(
-        &self,
-        min_len_wid_ratio: f32,
-        width_threshold: f32,
-        angle_threshold: f32,
-    ) -> Self {
-        let travelled_min_length_cell = Cell::new(0.0_f32);
+	pub fn cleanup_vertices(
+		&self,
+		min_len_wid_ratio: f32,
+		width_threshold: f32,
+		angle_threshold: f32,
+	) -> Self {
+		let travelled_min_length_cell = Cell::new(0.0_f32);
 
-        self.flat_map_with_prev_next(|curr, prev, next| {
-            if prev.is_none() || next.is_none() {
-                return vec![curr.clone()];
-            }
+		self.flat_map_with_prev_next(|curr, prev, next| {
+			if prev.is_none() || next.is_none() {
+				return vec![curr.clone()];
+			}
 
-            let prev = prev.unwrap();
-            let next = next.unwrap();
-            let travelled_min_length = travelled_min_length_cell.get();
-            let len = prev.len + curr.len + travelled_min_length;
-            let avg_width = (prev.width + curr.width * 2.0 + next.width) / 4.0;
+			let prev = prev.unwrap();
+			let next = next.unwrap();
+			let travelled_min_length = travelled_min_length_cell.get();
+			let len = prev.len + curr.len + travelled_min_length;
+			let avg_width = (prev.width + curr.width * 2.0 + next.width) / 4.0;
 
-            // handle min length, and skip vertices in between
+			// handle min length, and skip vertices in between
 
-            let min_len = f32::max(avg_width * min_len_wid_ratio, 1.0);
+			let min_len = f32::max(avg_width * min_len_wid_ratio, 1.0);
 
-            if len < min_len {
-                travelled_min_length_cell.set(travelled_min_length + prev.len);
-                return vec![];
-            }
+			if len < min_len {
+				travelled_min_length_cell.set(travelled_min_length + prev.len);
+				return vec![];
+			}
 
-            // TODO: Check if this is right!
-            if prev.len + travelled_min_length < min_len {
-                let dist = curr.len - (len - min_len);
-                let ratio = dist / curr.len;
-                travelled_min_length_cell.set(-dist);
-                return vec![curr.lerp(*next, ratio)];
-            }
+			// TODO: Check if this is right!
+			if prev.len + travelled_min_length < min_len {
+				let dist = curr.len - (len - min_len);
+				let ratio = dist / curr.len;
+				travelled_min_length_cell.set(-dist);
+				return vec![curr.lerp(*next, ratio)];
+			}
 
-            travelled_min_length_cell.set(0.0);
+			travelled_min_length_cell.set(0.0);
 
-            // handle unneeded vertices when similar width
-            // and similar direction as prev and next
+			// handle unneeded vertices when similar width
+			// and similar direction as prev and next
 
-            let is_same_width_prev =
-                prev.width == curr.width || (1.0 - prev.width / curr.width).abs() < width_threshold;
-            let is_same_width_next =
-                curr.width == next.width || (1.0 - next.width / curr.width).abs() < width_threshold;
-            let is_same_direction = 1.0 - prev.dir.dot(curr.dir) < angle_threshold;
+			let is_same_width_prev =
+				prev.width == curr.width || (1.0 - prev.width / curr.width).abs() < width_threshold;
+			let is_same_width_next =
+				curr.width == next.width || (1.0 - next.width / curr.width).abs() < width_threshold;
+			let is_same_direction = 1.0 - prev.dir.dot(curr.dir) < angle_threshold;
 
-            if is_same_width_next && is_same_width_prev && is_same_direction {
-                return vec![];
-            }
+			if is_same_width_next && is_same_width_prev && is_same_direction {
+				return vec![];
+			}
 
-            return vec![curr.clone()];
-        })
-    }
+			return vec![curr.clone()];
+		})
+	}
 }
 
 impl<'a, T> IntoIterator for &'a LineData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    type Item = &'a LineVertexData<T>;
-    type IntoIter = Iter<'a, LineVertexData<T>>;
+	type Item = &'a LineVertexData<T>;
+	type IntoIter = Iter<'a, LineVertexData<T>>;
 
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter()
-    }
+	fn into_iter(self) -> Self::IntoIter {
+		self.iter()
+	}
 }
 
 impl<T> FromIterator<LineVertexData<T>> for LineData<T>
 where
-    T: Default + Copy + Clone + Lerp<f32>,
+	T: Default + Copy + Clone + Lerp<f32>,
 {
-    fn from_iter<I: IntoIterator<Item = LineVertexData<T>>>(iter: I) -> Self {
-        let mut line = LineData::new(1.0);
-        for vert in iter {
-            line.add_vert(vert);
-        }
-        line
-    }
+	fn from_iter<I: IntoIterator<Item = LineVertexData<T>>>(iter: I) -> Self {
+		let mut line = LineData::new(1.0);
+		for vert in iter {
+			line.add_vert(vert);
+		}
+		line
+	}
 }
 
 pub mod buffered_geometry;
--- src/wasm_helpers/mod.rs
+++ src/wasm_helpers/mod.rs
@@ -2,68 +2,68 @@ use glam::{Mat2, Mat3, Mat4, Vec2, Vec3, Vec4};
 use js_sys::Float32Array;
 
 pub fn mat2_to_js(mat: &Mat2) -> Float32Array {
-    js_sys::Float32Array::from(mat.to_cols_array().as_slice())
+	js_sys::Float32Array::from(mat.to_cols_array().as_slice())
 }
 
 pub fn mat3_to_js(mat: &Mat3) -> Float32Array {
-    js_sys::Float32Array::from(mat.to_cols_array().as_slice())
+	js_sys::Float32Array::from(mat.to_cols_array().as_slice())
 }
 
 pub fn mat4_to_js(mat: &Mat4) -> Float32Array {
-    js_sys::Float32Array::from(mat.to_cols_array().as_slice())
+	js_sys::Float32Array::from(mat.to_cols_array().as_slice())
 }
 
 pub fn vec2_to_js(vec: &Vec2) -> Float32Array {
-    js_sys::Float32Array::from(vec.to_array().as_slice())
+	js_sys::Float32Array::from(vec.to_array().as_slice())
 }
 
 pub fn vec3_to_js(vec: &Vec3) -> Float32Array {
-    js_sys::Float32Array::from(vec.to_array().as_slice())
+	js_sys::Float32Array::from(vec.to_array().as_slice())
 }
 
 pub fn vec4_to_js(vec: &Vec4) -> Float32Array {
-    js_sys::Float32Array::from(vec.to_array().as_slice())
+	js_sys::Float32Array::from(vec.to_array().as_slice())
 }
 
 #[macro_export]
 macro_rules! setup_camera_interactions {
-    ($state_struct: ident, $camera_identifier:ident) => {
-        #[wasm_bindgen]
-        pub fn update_screen(width: f32, height: f32) {
-            $state_struct::mutate(|s| s.$camera_identifier.set_aspect_ratio(width / height))
-        }
+	($state_struct: ident, $camera_identifier:ident) => {
+		#[wasm_bindgen]
+		pub fn update_screen(width: f32, height: f32) {
+			$state_struct::mutate(|s| s.$camera_identifier.set_aspect_ratio(width / height))
+		}
 
-        #[derive(Serialize)]
-        struct CameraTransform {
-            pos: Vec3,
-            rot_horizontal: f32,
-            rot_vertical: f32,
-        }
+		#[derive(Serialize)]
+		struct CameraTransform {
+			pos: Vec3,
+			rot_horizontal: f32,
+			rot_vertical: f32,
+		}
 
-        #[wasm_bindgen]
-        pub fn update_camera(forward: f32, left: f32, up: f32, rot_y: f32, rot_x: f32) -> JsValue {
-            $state_struct::mutate(|s| {
-                s.$camera_identifier
-                    .update_transform(forward, left, up, rot_y, rot_x)
-            });
-            let s = $state_struct::read();
-            serde_wasm_bindgen::to_value(&CameraTransform {
-                pos: s.$camera_identifier.translation,
-                rot_horizontal: s.$camera_identifier.rot_horizontal,
-                rot_vertical: s.$camera_identifier.rot_vertical,
-            })
-            .unwrap()
-        }
+		#[wasm_bindgen]
+		pub fn update_camera(forward: f32, left: f32, up: f32, rot_y: f32, rot_x: f32) -> JsValue {
+			$state_struct::mutate(|s| {
+				s.$camera_identifier
+					.update_transform(forward, left, up, rot_y, rot_x)
+			});
+			let s = $state_struct::read();
+			serde_wasm_bindgen::to_value(&CameraTransform {
+				pos: s.$camera_identifier.translation,
+				rot_horizontal: s.$camera_identifier.rot_horizontal,
+				rot_vertical: s.$camera_identifier.rot_vertical,
+			})
+			.unwrap()
+		}
 
-        #[wasm_bindgen]
-        pub fn reset_camera(x: f32, y: f32, z: f32, rot_horizontal: f32, rot_vertical: f32) {
-            $state_struct::mutate(|s| {
-                s.$camera_identifier.reset_transform(
-                    vec3(x as f32, y as f32, z as f32),
-                    rot_horizontal,
-                    rot_vertical,
-                )
-            })
-        }
-    };
+		#[wasm_bindgen]
+		pub fn reset_camera(x: f32, y: f32, z: f32, rot_horizontal: f32, rot_vertical: f32) {
+			$state_struct::mutate(|s| {
+				s.$camera_identifier.reset_transform(
+					vec3(x as f32, y as f32, z as f32),
+					rot_horizontal,
+					rot_vertical,
+				)
+			})
+		}
+	};
 }
