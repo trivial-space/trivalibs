3b6a74e33c540c5d195c410834d86df6f13af12f
Thomas Gorny <thomas.gorny@pm.me>
2024-12-14 23:04:32 +0100

    more cleaning of examples

--- examples/triangle/shader/src/lib.rs
+++ examples/triangle/shader/src/lib.rs
@@ -1,40 +1,44 @@
 #![no_std]
-use spirv_std::{
-	glam::{vec2, vec4, Vec2, Vec4},
-	spirv,
-};
+#![allow(unexpected_cfgs)]
 
-pub const CLIP_SPACE_COORD_QUAD_CCW: [Vec4; 6] = {
-	let tl = vec4(-1.0, 1.0, 0.5, 1.0);
-	let tr = vec4(1.0, 1.0, 0.5, 1.0);
-	let bl = vec4(-1.0, -1.0, 0.5, 1.0);
-	let br = vec4(1.0, -1.0, 0.5, 1.0);
-	[bl, br, tr, tr, tl, bl]
-};
+#[cfg(target_arch = "spirv")]
+use spirv_std::glam::{vec4, Vec2, Vec3, Vec4};
+use spirv_std::{glam::vec2, spirv, Image, Sampler};
+#[cfg(not(target_arch = "spirv"))]
+use trivalibs::glam::{vec4, Vec2, Vec3, Vec4};
+#[cfg(not(target_arch = "spirv"))]
+use trivalibs::macros::*;
 
-pub const UV_COORD_QUAD_CCW: [Vec2; 6] = {
-	let tl = vec2(0.0, 0.0);
-	let tr = vec2(1.0, 0.0);
-	let bl = vec2(0.0, 1.0);
-	let br = vec2(1.0, 1.0);
-	[bl, br, tr, tr, tl, bl]
-};
-
-/// Vertex shader that renders an implicit quad.
-#[spirv(vertex)]
-pub fn vertex(
-	#[spirv(vertex_index)] vertex_id: u32,
-	out_uv: &mut Vec2,
-	#[spirv(position)] clip_pos: &mut Vec4,
-) {
-	let index = vertex_id as usize % 6;
-	*out_uv = UV_COORD_QUAD_CCW[index];
-	*clip_pos = CLIP_SPACE_COORD_QUAD_CCW[index];
+#[cfg(not(target_arch = "spirv"))]
+#[apply(gpu_data)]
+pub struct Vertex {
+	pub position: Vec3,
+	pub color: Vec3,
+	pub uv: Vec2,
 }
 
-/// Fragment shader that uses UV coords passed in from the vertex shader
-/// to render a simple gradient.
+#[spirv(vertex)]
+pub fn vertex(
+	position: Vec3,
+	color: Vec3,
+	uv: Vec2,
+	#[spirv(position)] clip_pos: &mut Vec4,
+	out_color: &mut Vec3,
+	out_uv: &mut Vec2,
+) {
+	*out_color = color;
+	*out_uv = uv;
+	*clip_pos = position.extend(1.0);
+}
+
 #[spirv(fragment)]
-pub fn fragment(in_uv: Vec2, frag_color: &mut Vec4) {
-	*frag_color = vec4(in_uv.x, 1.0 - in_uv.y, 0.0, 1.0);
+pub fn fragment(
+	in_color: Vec3,
+	in_uv: Vec2,
+	#[spirv(descriptor_set = 0, binding = 0)] tex: &Image!(2D, type=f32, sampled),
+	#[spirv(descriptor_set = 0, binding = 1)] sampler: &Sampler,
+	frag_color: &mut Vec4,
+) {
+	let col = tex.sample(*sampler, vec2(in_uv.x, in_uv.y));
+	*frag_color = vec4(col.x, col.y, col.z, 1.0) * in_color.extend(1.0);
 }
