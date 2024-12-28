f3d4613db14b2362afd267a06ac2d84e2339b005
Thomas Gorny <thomas.gorny@pm.me>
2024-12-28 22:52:01 +0100

    first render of blur example

--- examples/blur/src/main.rs
+++ examples/blur/src/main.rs
@@ -1,8 +1,19 @@
 use trivalibs::{
+	bmap,
 	glam::{vec2, Vec2},
 	gpu_data,
 	macros::apply,
-	painter::{layer::Layer, wgpu, CanvasApp, Event, Painter},
+	painter::{
+		effect::EffectProps,
+		layer::{Layer, LayerProps},
+		load_fragment_shader, load_vertex_shader,
+		shade::{ShadeEffectProps, ShadeProps},
+		sketch::SketchProps,
+		uniform::UniformBuffer,
+		wgpu::{self, VertexFormat::*},
+		CanvasApp, Event, Painter,
+	},
+	utils::default,
 };
 
 #[apply(gpu_data)]
@@ -28,28 +39,77 @@ const TRIANGLE: &[Vertex] = &[
 
 struct App {
 	canvas: Layer,
+	triangle: Layer,
+	size: UniformBuffer<Vec2>,
 }
 
 impl CanvasApp<()> for App {
 	fn init(p: &mut Painter) -> Self {
-		todo!()
+		let u_fs_type = p.uniform_type_buffered_frag();
+		let tex_type = p.uniform_type_tex_2d_frag();
+
+		let triangle_shade = p.shade_create(ShadeProps {
+			uniform_types: &[],
+			vertex_format: &[Float32x2, Float32x2],
+		});
+		load_vertex_shader!(triangle_shade, p, "../triangle_shader/vert.spv");
+		load_fragment_shader!(triangle_shade, p, "../triangle_shader/frag.spv");
+
+		let blur_shade = p.shade_create_effect(ShadeEffectProps {
+			uniform_types: &[tex_type, u_fs_type],
+		});
+		load_fragment_shader!(blur_shade, p, "../blur_shader/frag.spv");
+
+		let tri_form = p.form_create(TRIANGLE, default());
+
+		let tri_sketch = p.sketch_create(tri_form, triangle_shade, SketchProps { ..default() });
+
+		let triangle = p.layer_create(LayerProps {
+			sketches: vec![tri_sketch],
+			clear_color: Some(wgpu::Color::BLUE),
+			..default()
+		});
+
+		let tex = triangle.get_uniform(p, p.sampler_default());
+		let size = u_fs_type.create_vec2(p);
+
+		let effect = p.effect_create(
+			blur_shade,
+			EffectProps {
+				uniforms: bmap! {
+					0 => tex.uniform,
+					1 => size.uniform,
+				},
+				..default()
+			},
+		);
+
+		let canvas = p.layer_create(LayerProps {
+			effects: vec![effect],
+			..default()
+		});
+
+		Self {
+			canvas,
+			triangle,
+			size,
+		}
 	}
 
 	fn resize(&mut self, p: &mut Painter) {
-		todo!()
-	}
-
-	fn update(&mut self, p: &mut Painter, tpf: f32) {
-		todo!()
+		let size = p.canvas_size();
+		self.size
+			.update(p, vec2(size.width as f32, size.height as f32));
 	}
 
 	fn render(&self, p: &mut Painter) -> Result<(), wgpu::SurfaceError> {
-		todo!()
+		p.paint(&self.canvas)?;
+		p.paint(&self.triangle)?;
+		p.show(&self.canvas)
 	}
 
-	fn event(&mut self, event: Event<()>, p: &mut Painter) {
-		todo!()
-	}
+	fn update(&mut self, _p: &mut Painter, _tpf: f32) {}
+	fn event(&mut self, _e: Event<()>, _p: &mut Painter) {}
 }
 
 pub fn main() {
