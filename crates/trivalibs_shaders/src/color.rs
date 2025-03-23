ce81b2cd3644d68e0368e00c6f2d4362ca230e8a
Thomas Gorny <thomas.gorny@pm.me>
2025-03-23 14:19:35 +0100

    add hsv2rgb_smooth

--- crates/trivalibs_shaders/src/color.rs
+++ crates/trivalibs_shaders/src/color.rs
@@ -1,17 +1,4 @@
-// vec3 rgb2hsl( in vec3 c ){
-//     vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-//     vec4 p = mix(vec4(c.bg, K.wz),
-//                  vec4(c.gb, K.xy),
-//                  step(c.b, c.g));
-//     vec4 q = mix(vec4(p.xyw, c.r),
-//                  vec4(c.r, p.yzx),
-//                  step(p.x, c.r));
-//     float d = q.x - min(q.w, q.y);
-//     float e = 1.0e-10;
-//     return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)),
-//                 d / (q.x + e),
-//                 q.x);
-// }
+#![allow(unused_imports)]
 
 use crate::{smoothstep::Smoothen, step::Step};
 use spirv_std::glam::{vec3, vec4, Vec3};
@@ -33,7 +20,13 @@ pub fn rgb2hsl(c: Vec3) -> Vec3 {
 //  Function from Iñigo Quiles
 //  https://www.shadertoy.com/view/MsS3Wc
 
-pub fn hsl2rgb(c: Vec3) -> Vec3 {
-	let rgb = (((c.x * 6.0 + vec3(0.0, 4.0, 2.0) % 6.0) - 3.0).abs() - 1.0).smoothen();
+pub fn hsv2rgb(c: Vec3) -> Vec3 {
+	let rgb = ((((c.x * 6.0 + vec3(0.0, 4.0, 2.0)) % 6.0) - 3.0).abs() - 1.0)
+		.clamp(Vec3::ZERO, Vec3::ONE);
+	c.z * Vec3::ONE.lerp(rgb, c.y)
+}
+
+pub fn hsv2rgb_smooth(c: Vec3) -> Vec3 {
+	let rgb = ((((c.x * 6.0 + vec3(0.0, 4.0, 2.0)) % 6.0) - 3.0).abs() - 1.0).smoothen();
 	c.z * Vec3::ONE.lerp(rgb, c.y)
 }
